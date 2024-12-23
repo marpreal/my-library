91ec112019e4ea4d817d4b526113678c71523aca
Marta Pretel <marpreal97@gmail.com>
2024-12-23 13:06:34 +0100

    fix types

--- src/app/api/books/route.ts
+++ src/app/api/books/route.ts
@@ -1,7 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 import { NextResponse } from 'next/server';
 
-
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 const prisma =
@@ -19,14 +18,14 @@ export async function POST(request: Request) {
 
   try {
     const newBook = await prisma.book.create({
-      data: { titulo, autor }, // Eliminamos userId
+      data: { titulo, autor },
     });
 
     return NextResponse.json(newBook, { status: 201 });
   } catch (error) {
-    console.error('Error al crear el libro:', error.message, error.stack);
+    console.error('Error al crear el libro:', (error as Error).message);
     return NextResponse.json(
-      { error: error.message || 'Error al crear el libro' },
+      { error: (error as Error).message || 'Error al crear el libro' },
       { status: 500 }
     );
   }
