a04b77097512ed2b2206f8d85c40910201491975
Marta Pretel <marpreal97@gmail.com>
2024-12-23 11:23:04 +0100

    Fix Prisma Client generation on Vercel

--- src/app/api/books/route.ts
+++ src/app/api/books/route.ts
@@ -1,7 +1,18 @@
 import { PrismaClient } from '@prisma/client';
 import { NextResponse } from 'next/server';
 
-const prisma = new PrismaClient();
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['query'], // Opcional: Log de consultas para debug
+  });
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+
+export default prisma;
 
 export async function POST(request: Request) {
   const { titulo, autor } = await request.json();
